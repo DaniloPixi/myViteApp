775000371265eafe102577589fae4c22d3509806
Pixi10 <noxus010@gmail.com>
2025-09-01 22:49:30 +0200

    notif test 4

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,13 +1,13 @@
 // public/firebase-messaging-sw.js
-// Import and initialize the Firebase SDK using the "compat" versions
+
+// Precache and route the files injected by the Vite PWA plugin.
+precacheAndRoute(self.__WB_MANIFEST || []);
+
+// The rest of your Firebase code...
 importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/9.1.0/firebase-messaging-compat.js');
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-precaching.prod.js');
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
 
-const { precacheAndRoute } = workbox.precaching;
-precacheAndRoute(self.__WB_MANIFEST || []);
+// Initialize the Firebase app in the service worker
 const firebaseConfig = {
 apiKey: "AIzaSyCyOF7Pt2SVD0Q6G-nr4TMBR1crFDeIjpA",
   authDomain: "gruscorner.firebaseapp.com",
@@ -26,11 +26,10 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
 
-  // Customize the notification
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/pwa-512x512.png' // Use one of your app icons
+    icon: '/pwa-512x512.png'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
