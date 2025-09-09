e4af8e7bff2da882e0719b804304ca3a441685a6
noxus010 <noxus010@gmail.com>
2025-09-09 11:06:34 +0000

    app icon changed

--- vite.config.js
+++ vite.config.js
@@ -41,17 +41,17 @@ export default defineConfig(({ command }) => {
           theme_color: '#42b883',
           icons: [
             {
-              src: 'pwa-192x192.png',
+              src: 'manifest-icon-192.maskable.png',
               sizes: '192x192',
               type: 'image/png'
             },
             {
-              src: 'pwa-512x512.png',
+              src: 'manifest-icon-512.maskable.png',
               sizes: '512x512',
               type: 'image/png'
             },
             {
-              src: 'pwa-512x512.png',
+              src: 'manifest-icon-512.maskable.png',
               sizes: '512x512',
               type: 'image/png',
               purpose: 'any maskable'
